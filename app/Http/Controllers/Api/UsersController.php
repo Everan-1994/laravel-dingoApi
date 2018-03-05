251f96c378e632a9fc12714c2d41362d2c47ebf2
vagrant <vagrant@homestead>
2018-03-05 09:22:55 +0000

    api/user

--- app/Http/Controllers/Api/UsersController.php
+++ app/Http/Controllers/Api/UsersController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Api;
 
 use App\Models\User;
 use App\Http\Requests\Api\UserRequest;
+use App\Transformers\UserTransformer;
 
 class UsersController extends Controller
 {
@@ -20,16 +21,27 @@ class UsersController extends Controller
             return $this->response->errorUnauthorized('验证码错误');
         }
 
-        User::create([
-            'name' => $request->name,
-            'phone' => $verifyData['phone'],
+        $user = User::create([
+            'name'     => $request->name,
+            'phone'    => $verifyData['phone'],
             'password' => bcrypt($request->password),
         ]);
 
         // 清除验证码缓存
         cache()->forget($request->verification_key);
 
-        return $this->response->created();
+        return $this->response->item($user, new UserTransformer())
+            ->setMeta([
+                'access_token' => auth()->guard('api')->fromUser($user),
+                'token_type'   => 'Bearer',
+                'expires_in'   => auth()->guard('api')->factory()->getTTL() * 60
+            ])->setStatusCode(201);
+    }
+
+    public function me()
+    {
+        // $this->user() 等同于 auth()->guard('api')->user(); 获取token对应的用户
+        return $this->response->item($this->user(), new UserTransformer());
     }
 
 }
